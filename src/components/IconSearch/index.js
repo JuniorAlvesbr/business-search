b9a92e53061d081e2c5a48c3e0757cb7fdee9496
Junior Alves <jrnalves@gmail.com>
2020-12-21 14:19:53 -0300

    :construction: Create IconSearch

---
+++ src/components/IconSearch/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import icon from '../../assets/images/icon-search.svg';
+
+const IconSearch = styled.img.attrs({
+  src: icon,
+  alt: 'icone de pesquisa no site',
+})``;
+
+export default IconSearch;
