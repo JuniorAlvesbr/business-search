afff67d74b234146c7f8629a0a5fc2d5704215d4
Junior Alves <jrnalves@gmail.com>
2020-12-21 16:15:37 -0300

    :lipstick: update margin right

--- src/components/ServiceFilter/styles.js
+++ src/components/ServiceFilter/styles.js
@@ -1,13 +1,17 @@
 import styled from 'styled-components';
+import IconMenu from '../IconMenu';
 
 export const ServiceFilterStyle = styled.nav`
   display: flex;
   background-color: #3a4042;
+
+  & > ${IconMenu} {
+    margin-right: 16px;
+  }
 `;
 
 export const Label = styled.p`
   font-family: 'Comfortaa', cursive;
   font-size: 20px;
   color: white;
-  margin-left: 16px;
 `;
