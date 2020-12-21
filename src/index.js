0483020de01e5c72b82214fbeeb2f96dd6064411
Junior Alves <jrnalves@gmail.com>
2020-12-21 16:34:56 -0300

    :sparkles: create reset CSS with styled-components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Reset from './styles/generic/Reset';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
