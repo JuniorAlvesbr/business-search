3879fc60e15a21d110c8cc59f91389e62903752e
Junior Alves <jrnalves@gmail.com>
2020-12-21 16:10:16 -0300

    :sparkles: create Service Guide

---
+++ src/components/ServiceFilter/index.js
@@ -0,0 +1,13 @@
+import { ServiceFilterStyle, Label } from './styles';
+import IconMenu from '../IconMenu';
+
+function ServiceFilter() {
+  return (
+    <ServiceFilterStyle>
+      <IconMenu />
+      <Label>Guia de Serviços</Label>
+    </ServiceFilterStyle>
+  );
+}
+
+export default ServiceFilter;
---
+++ src/components/ServiceFilter/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ServiceFilterStyle = styled.nav`
+  display: flex;
+  background-color: #3a4042;
+`;
+
+export const Label = styled.p`
+  font-family: 'Comfortaa', cursive;
+  font-size: 20px;
+  color: white;
+  margin-left: 16px;
+`;
