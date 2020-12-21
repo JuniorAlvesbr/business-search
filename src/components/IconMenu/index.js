47603b210f484dbf78f333dc022f666bde7675d5
Junior Alves <jrnalves@gmail.com>
2020-12-21 16:08:12 -0300

    :sparkles: create icon-menu component

---
+++ src/components/IconMenu/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import icon from '../../assets/images/icon-menu.svg';
+
+const IconMenu = styled.img.attrs({
+  src: icon,
+  alt: 'icone do menu fechado',
+})``;
+
+export default IconMenu;
