89e723ca35f32e2437d38a5fd128a5a2fe7db796
Junior Alves <jrnalves@gmail.com>
2020-12-26 18:20:36 -0300

    :sparkles: add ServiceFilter in storybook

---
+++ src/stories/components/ServiceFilter.stories.js
@@ -0,0 +1,8 @@
+import ServiceFilter from '../../components/ServiceFilter'
+
+export default {
+    title: 'Components/ServiceFilter',
+    component: 'ServiceFilter'
+}
+
+export const Default = () => <ServiceFilter />
