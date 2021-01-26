072c6a874f5c9fc6be7202e31c4d49789fdf6783
Junior Alves <jrnalves@gmail.com>
2021-01-26 19:21:09 -0300

    :sparkles create iconClose:

---
+++ src/components/IconClose/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import icon from '../../assets/images/icon-close.svg';
+
+const IconClose = styled.img.attrs({
+    src: icon,
+    alt: "icone de fechar o menu"
+})``;
+
+export default IconClose;
---
+++ src/stories/components/IconClose.stories.js
@@ -0,0 +1,11 @@
+import IconClose from '../../components/IconClose';
+
+export default {
+    title: 'Components/IconClose',
+    components: IconClose,
+    parameters: {
+        layout: "centered"
+    }
+};
+
+export const Default = () => <IconClose />
