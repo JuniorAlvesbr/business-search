58ad2c1a36976a4d6094d3f47a2de35998928e72
Junior Alves <jrnalves@gmail.com>
2021-02-01 11:54:22 -0300

    create details page and add mainHeader at the top

---
+++ src/pages/Details/index.js
@@ -0,0 +1,14 @@
+import HeaderMain from "../../components/HeaderMain";
+import LogoMain from '../../components/LogoMain';
+import IconSearch from "../../components/IconSearch";
+
+function Details() {
+    return (
+        <HeaderMain>
+            <LogoMain />
+            <IconSearch />
+        </HeaderMain>
+    )
+}
+
+export default Details;
