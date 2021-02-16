79302674aa2170e4c72ad1b984a7d0277094425f
Junior Alves <jrnalves@gmail.com>
2021-02-16 18:46:02 -0300

    add Stars components

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,11 +1,13 @@
 import HeaderMain from "../../components/HeaderMain";
 import BackAction from "../../components/BackAction";
+import Stars from "../../components/Stars";
 
 function Details() {
     return (
         <>
             <HeaderMain />
             <BackAction />
+            <Stars amount="5" />
         </>
     )
 }
