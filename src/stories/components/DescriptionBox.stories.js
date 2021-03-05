bc3214fa7f4a74457c0dacdf7e7f088d14e31fab
Junior Alves <jrnalves@gmail.com>
2021-03-05 17:43:46 -0300

    add props in storybook of the DescriptionBox

--- src/stories/components/DescriptionBox.stories.js
+++ src/stories/components/DescriptionBox.stories.js
@@ -8,10 +8,10 @@ export default {
     }
 }
 
-export const Default = () => (
-    <DescriptionBox>
-        Aqui é só felicidade e café com um bom sol no verão e no inverno.
-        Gostosas sopas para deixar o seu dia mais quente e feliz.
-        Aqui o proprietário poderá escrever o que quiser sobre o seu café;
-    </DescriptionBox>
-);
+const Template = args => <DescriptionBox {...args} />
+
+export const Default = Template.bind({});
+
+Default.args = {
+    children: "Aqui é só felicidade e café com um bom sol no verão e no inverno. Gostosas sopas para deixar o seu dia mais quente e feliz. Aqui o proprietário poderá escrever o que quiser sobre o seu café;",
+};
