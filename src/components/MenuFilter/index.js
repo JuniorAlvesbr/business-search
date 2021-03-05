1da1a9caf81451e10ede36233dd533262043d224
Junior Alves <jrnalves@gmail.com>
2021-03-05 20:09:04 -0300

    add props of the MenuFilter

--- src/components/MenuFilter/index.js
+++ src/components/MenuFilter/index.js
@@ -1,24 +1,10 @@
 import MenuFilterStyle from "./styles";
 import CheckBox from '../CheckBox';
 
-function MenuFilter({ active }) {
+function MenuFilter({ active, items }) {
     return (
         <MenuFilterStyle active={active}>
-            <CheckBox id="alimentacao" label="Alimentação" />
-            <CheckBox id="animais" label="Animais" />
-            <CheckBox id="arteCultura" label="Arte e Cultura" />
-            <CheckBox id="entidades" label="Entidades" />
-            <CheckBox id="audioVideo" label="Audio e Video" />
-            <CheckBox id="automoveis" label="Automoveis" />
-            <CheckBox id="bancos" label="Bancos" />
-            <CheckBox id="bareseBebidas" label="Bares e Bebidas" />
-            <CheckBox id="belezaEstetica" label="Beleza e Estética" />
-            <CheckBox id="cafes" label="Cafés" />
-            <CheckBox id="crinaçasBebes" label="Crianças e Bebes" />
-            <CheckBox id="diversaoLazer" label="Diversão e Lazer" />
-            <CheckBox id="escritorios" label="Escritórios" />
-            <CheckBox id="esporte" label="Esporte" />
-            <CheckBox id="fastFood" label="Fast Food" />
+            {items.map(({ id, label }) => <CheckBox id={id} label={label} />)}
         </MenuFilterStyle>
     )
 }
--- src/stories/components/MenuFilter.stories.js
+++ src/stories/components/MenuFilter.stories.js
@@ -5,14 +5,35 @@ export default {
     components: MenuFilter
 };
 
+const items = [
+    { id: "alimentacao", label: "Alimentação" },
+    { id: "animais", label: "Animais" },
+    { id: "arteCultura", label: "Arte e Cultura" },
+    { id: "entidades", label: "Entidades" },
+    { id: "audioVideo", label: "Audio e Video" },
+    { id: "automoveis", label: "Automoveis" },
+    { id: "bancos", label: "Bancos" },
+    { id: "bareseBebidas", label: "Bares e Bebidas" },
+    { id: "belezaEstetica", label: "Beleza e Estética" },
+    { id: "cafes", label: "Cafés" },
+    { id: "crinaçasBebes", label: "Crianças e Bebes" },
+    { id: "diversaoLazer", label: "Diversão e Lazer" },
+    { id: "escritorios", label: "Escritórios" },
+    { id: "esporte", label: "Esporte" },
+    { id: "fastFood", label: "Fast Food" }
+]
+
+
 const Template = (args) => <MenuFilter {...args} />;
 
 export const Open = Template.bind({});
 Open.args = {
-    active: true
+    active: true,
+    items
 };
 
 export const Close = Template.bind({});
 Close.args = {
-    active: false
+    active: false,
+    items
 };
