9c8671ca9ace715760f89aac5ba24b0582cb3627
Junior Alves <jrnalves@gmail.com>
2021-03-06 05:47:54 -0300

    Add props in storybook of the Stars

--- src/stories/components/Star.stories.js
+++ src/stories/components/Star.stories.js
@@ -8,8 +8,29 @@ export default {
     }
 }
 
-export const OneStar = () => <Stars amount='1' />
-export const TwoStars = () => <Stars amount='2' />
-export const ThreeStars = () => <Stars amount='3' />
-export const FourStars = () => <Stars amount='4' />
-export const FiveStars = () => <Stars amount='5' />
+const Template = args => <Stars {...args} />
+
+export const OneStar = Template.bind({});
+OneStar.args = {
+    amount: 1
+}
+
+export const TwoStar = Template.bind({});
+TwoStar.args = {
+    amount: 2
+}
+
+export const TreeStar = Template.bind({});
+TreeStar.args = {
+    amount: 3
+}
+
+export const FourStar = Template.bind({});
+FourStar.args = {
+    amount: 4
+}
+
+export const FiveStar = Template.bind({});
+FiveStar.args = {
+    amount: 5
+}
