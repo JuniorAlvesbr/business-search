9f69a8fd06ca8c2968e6d5ed3a686e58fc478128
Junior Alves <jrnalves@gmail.com>
2021-03-06 16:41:03 -0300

    add props in components

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import HeaderMain from "../../components/HeaderMain";
 import BackAction from "../../components/BackAction";
 import WrapperBusiness from '../../components/WrapperBusiness';
@@ -6,8 +7,12 @@ function Details() {
     return (
         <>
             <HeaderMain />
-            <BackAction />
-            <WrapperBusiness />
+            <BackAction as={Link} content="Voltar" />
+            <WrapperBusiness
+                stars="5"
+                title="Café do Zé"
+                description="Aqui é só felicidade e café com um bom sol no verão e no inverno"
+            />
         </>
     )
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,21 +2,55 @@ import HeaderMain from '../../components/HeaderMain';
 import ServiceFilter from '../../components/ServiceFilter';
 import ButtonFixed from '../../components/ButtonFixed';
 import ServiceWrapper from '../../components/ServiceWrapper';
-import CardService from '../../components/CardService';
 
 function Home() {
+
+    const items = [
+        { id: "alimentacao", label: "Alimentação" },
+        { id: "animais", label: "Animais" },
+        { id: "arteCultura", label: "Arte e Cultura" },
+        { id: "entidades", label: "Entidades" },
+        { id: "audioVideo", label: "Audio e Video" },
+        { id: "automoveis", label: "Automoveis" },
+        { id: "bancos", label: "Bancos" },
+        { id: "bareseBebidas", label: "Bares e Bebidas" },
+        { id: "belezaEstetica", label: "Beleza e Estética" },
+        { id: "cafes", label: "Cafés" },
+        { id: "crinaçasBebes", label: "Crianças e Bebes" },
+        { id: "diversaoLazer", label: "Diversão e Lazer" },
+        { id: "escritorios", label: "Escritórios" },
+        { id: "esporte", label: "Esporte" },
+        { id: "fastFood", label: "Fast Food" }
+    ]
+
+    const servicesList = [
+        {
+            featuredContent: "Em Destaque",
+            featured: true,
+            stars: 5,
+            title: "Cafeteria D’ Arte"
+        },
+        {
+            featuredContent: "Em Destaque",
+            featured: false,
+            stars: 3,
+            title: "Cafeteria Pedro"
+        },
+        {
+            featuredContent: "Em Destaque",
+            featured: false,
+            stars: 1,
+            title: "Cafeteria jose"
+        },
+    ]
+
     return (
         <>
             <HeaderMain />
 
-            <ServiceFilter />
+            <ServiceFilter label="Guia de Serviços" items={items} />
 
-            <ServiceWrapper>
-                <CardService />
-                <CardService />
-                <CardService />
-                <CardService />
-            </ServiceWrapper>
+            <ServiceWrapper list={servicesList} />
 
             <ButtonFixed>Insira o seu Negócio</ButtonFixed>
         </>
