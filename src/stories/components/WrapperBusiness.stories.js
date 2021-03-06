a079ee73c7b496fecd7d0738f4ac9a48a5099099
Junior Alves <jrnalves@gmail.com>
2021-03-06 15:15:16 -0300

    add props in storybook of the WrapperBusiness

--- src/stories/components/WrapperBusiness.stories.js
+++ src/stories/components/WrapperBusiness.stories.js
@@ -8,4 +8,12 @@ export default {
     }
 }
 
-export const Default = () => <WrapperBusiness />
+const Template = args => <WrapperBusiness {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+    stars: 5,
+    title: "Cafeteria D’ Arte",
+    description: "Aqui é só felicidade e café com um bom sol no verão e no inverno. Gostosas sopas para deixar o seu dia mais quente e feliz. Aqui o proprietário poderá escrever o que quiser sobre o seu café;"
+}
