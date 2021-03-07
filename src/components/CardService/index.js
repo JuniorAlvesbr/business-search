28b7e8897a43be69386cbe9ed8a0c673fb9282ea
Junior Alves <jrnalves@gmail.com>
2021-03-07 10:21:21 -0300

    bug - fixed bug in CardService

--- src/components/CardService/index.js
+++ src/components/CardService/index.js
@@ -1,22 +1,20 @@
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { CardServiceStyle, Figure, Caption, Photo, Title } from './styles';
 import Stars from '../Stars';
 
 function CardService({ featured, featuredContent, stars, title }) {
     return (
         <CardServiceStyle>
-            <Router>
-                <Link to="/details">
-                    <Figure>
-                        <Photo />
-                        <Caption active={featured}>{featuredContent}</Caption>
-                    </Figure>
+            <Link to="/details">
+                <Figure>
+                    <Photo />
+                    <Caption active={featured}>{featuredContent}</Caption>
+                </Figure>
 
-                    <Stars amount={stars} />
+                <Stars amount={stars} />
 
-                    <Title>{title}</Title>
-                </Link>
-            </Router>
+                <Title>{title}</Title>
+            </Link>
         </CardServiceStyle >
     );
 };
