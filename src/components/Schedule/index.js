d146d35a6c2bfff1644a92446e1fc51d86d44ae4
Junior Alves <jrnalves@gmail.com>
2021-03-25 20:33:20 -0300

    create schedule component

---
+++ src/components/Schedule/index.js
@@ -0,0 +1,13 @@
+import InfoOpenClose from '../InfoOpenClose'
+import OpeningHours from '../OpeningHours'
+
+function Schedule() {
+    return (
+        <>
+            <InfoOpenClose>Aberto Agora</InfoOpenClose>
+            <OpeningHours />
+        </>
+    )
+}
+
+export default Schedule
---
+++ src/stories/components/Schedule.stories.js
@@ -0,0 +1,11 @@
+import Schedule from '../../components/Schedule'
+
+export default {
+    title: 'Components/Schedule',
+    components: Schedule,
+    parameters: {
+        layout: 'centered'
+    }
+}
+
+export const Default = () => <Schedule />
