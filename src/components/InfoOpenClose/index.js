32de3ee9b240ce15e6f44fa3c850d446a40e88dc
Junior Alves <jrnalves@gmail.com>
2021-03-25 20:32:16 -0300

    create InfoOpenClose Components

---
+++ src/components/InfoOpenClose/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const InfoOpenClose = styled.p`
+    color: #62C370;
+    font-size: 24px;
+    margin-bottom: 10px;
+`
+
+export default InfoOpenClose;
+
---
+++ src/stories/components/InfoOpenClose.stories.js
@@ -0,0 +1,11 @@
+import InfoOpenClose from '../../components/InfoOpenClose'
+
+export default {
+    title: 'Components/InfoOpenClose',
+    component: InfoOpenClose,
+    parameters: {
+        layout: 'centered'
+    }
+}
+
+export const Default = () => <InfoOpenClose>Aberto Agora</InfoOpenClose>
