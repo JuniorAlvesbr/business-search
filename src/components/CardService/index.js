c85e410e3f81a9fb7091717e82c5834a78deb85e
Junior Alves <jrnalves@gmail.com>
2021-03-25 17:59:59 -0300

    fix link in to router

--- src/components/CardService/index.js
+++ src/components/CardService/index.js
@@ -1,21 +1,23 @@
-import { Link } from 'react-router-dom';
+import { BrowserRouter, Switch, Link } from 'react-router-dom';
 import { CardServiceStyle, Figure, Caption, Photo, Title } from './styles';
 import Stars from '../Stars';
 
 function CardService({ featured, featuredContent, stars, title }) {
     return (
         <CardServiceStyle>
-            <Link to="/details">
-                <Figure>
-                    <Photo />
-                    <Caption active={featured}>{featuredContent}</Caption>
-                </Figure>
+            <BrowserRouter>
+                <Link to="/details">
+                    <Figure>
+                        <Photo />
+                        <Caption active={featured}>{featuredContent}</Caption>
+                    </Figure>
 
-                <Stars amount={stars} />
+                    <Stars amount={stars} />
 
-                <Title>{title}</Title>
-            </Link>
-        </CardServiceStyle >
+                    <Title>{title}</Title>
+                </Link>
+            </BrowserRouter>
+        </CardServiceStyle>
     );
 };
 
