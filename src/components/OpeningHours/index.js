e8b5da0443154d1dd6190c82f093d72a5c2a6d60
Junior Alves <jrnalves@gmail.com>
2021-03-26 07:27:17 -0300

    refactory map function

--- src/components/OpeningHours/index.js
+++ src/components/OpeningHours/index.js
@@ -1,12 +1,15 @@
 import { OpeningHoursStyle, Items, Days, Hours } from './styles'
 
-function OpeningHours({ day, hours }) {
+function OpeningHours({ schedule }) {
+
     return (
         <OpeningHoursStyle>
-            <Items>
-                <Days>{day}</Days>
-                <Hours>{hours}</Hours>
-            </Items>
+            {schedule.map(item => (
+                <Items key={item.key}>
+                    <Days>{item.day}</Days>
+                    <Hours>{item.hours}</Hours>
+                </Items>
+            ))}
         </OpeningHoursStyle>
     )
 }
--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -6,20 +6,20 @@ function Schedule({ states }) {
     const stateNow = (states === "open" ? "Aberto Agora" : "Fechado Agora");
 
     const schedule = [
-        { key: "1", day: "Segunda", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "2", day: "Terça", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "3", day: "Quarta", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "4", day: "Quinta", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "5", day: "Sexta", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "6", day: "Sabado", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "7", day: "Domingo", hours: "08:00 - 12:00, 13:30 - 18:00" },
-        { key: "8", day: "Feriados", hours: "08:00 - 12:00, 13:30 - 18:00" },
+        { key: "1", day: "Segunda", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "2", day: "Terça", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "3", day: "Quarta", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "4", day: "Quinta", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "5", day: "Sexta", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "6", day: "Sabado", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "7", day: "Domingo", hours: "08:00 - 12:00 13:30 - 18:00" },
+        { key: "8", day: "Feriados", hours: "08:00 - 12:00 13:30 - 18:00" },
     ]
 
     return (
         <>
             <InfoOpenClose states={states} children={stateNow} />
-            {schedule.map(item => <OpeningHours {...item} />)}
+            <OpeningHours schedule={schedule} />
         </>
     )
 }
