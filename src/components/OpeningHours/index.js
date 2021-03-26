9e3fe604c52b108cf087c5b113e0b43ff14a7813
Junior Alves <jrnalves@gmail.com>
2021-03-25 22:05:23 -0300

    refactory, dynamic OpeningHours component

--- src/components/OpeningHours/index.js
+++ src/components/OpeningHours/index.js
@@ -1,11 +1,11 @@
 import { OpeningHoursStyle, Items, Days, Hours } from './styles'
 
-function OpeningHours() {
+function OpeningHours({ day, hours }) {
     return (
         <OpeningHoursStyle>
             <Items>
-                <Days>Segunda</Days>
-                <Hours>08:00 - 12:00 13:00 - 18:00</Hours>
+                <Days>{day}</Days>
+                <Hours>{hours}</Hours>
             </Items>
         </OpeningHoursStyle>
     )
